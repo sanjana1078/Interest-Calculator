669e41b7a6463db725df5edff02cefe16f4df6d3
Sanjana <sanjana.gupta@bds.christuniversity.in>
2024-04-19 15:14:12 +0530

    Advance Code

---
+++ interest_calculator/interest_calculator/calculator/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Calculation
+
+class calculationForm(forms.ModelForm):
+    class Meta:
+        model = Calculation
+        fields = ["principal", "rate", "time",]
+        
